dcc346040b1a45a84fe629033b5245600a8ac287
serj <13497925+gamedevserj@users.noreply.github.com>
2026-09-05 13:52:27 +0400

    Cleanup

--- NET/addons/layer_presets/PresetsController.cs
+++ NET/addons/layer_presets/PresetsController.cs
@@ -119,16 +119,6 @@ internal static class PresetsController
             ProjectSettings.SetSetting(GetLayersSetting(propertyHint), allPresets);
         }
 
-        //var propertyInfo = new Dictionary
-        //    {
-        //        { "name", SettingsConstants.PhysicsLayers3DPresetsSetting },
-        //        { "type", (int)Variant.Type.Dictionary },
-        //        { "hint", (int)PropertyHint.DictionaryType }, 
-        //        // https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html#enum-globalscope-propertyhint
-        //        // 11 is for the 3D Physics layer
-        //        { "hint_string", $"{(int)Variant.Type.String}:;{(int)Variant.Type.Int}/11:" }
-        //    };
-        //ProjectSettings.AddPropertyInfo(propertyInfo);
         Save(propertyHint);
     }
 
